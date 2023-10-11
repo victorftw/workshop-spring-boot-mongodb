44a63c5288a8865fdb406793bd747ce880438990
Victor Hugo <victor.hiraiftw@outlook.com>
2023-10-11 10:25:35 -0300

    initial commit

---
+++ src/main/java/com/victordev/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,16 @@
+package com.victordev.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * WorkshopmongoApplication is the main class that starts the Spring Boot application.
+ */
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorkshopmongoApplication.class, args);
+  }
+}
